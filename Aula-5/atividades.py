18331dec5ead8f7e8e879e6bf9b45b1f9280a12b
f8281948-coder <f8281948@gmail.com>
2026-07-22 20:17:01 -0300

    Add files via upload

---
+++ Aula-5/atividades.py
@@ -0,0 +1,121 @@
+# #Exercícios: 
+
+# UTILIZE VARIÁVEIS!!!!!! 
+# Utilize as 5 concatenações, teste todas 
+
+# print('olá ' + ' ' + '10')
+# # print(10 + 10)
+
+
+
+# # ,
+# print('olá ',  '10')
+
+
+
+# # % 
+
+
+# print('olá %d'%(10))
+
+
+
+# # .format()
+
+
+# print('olá  {}'.format(10))
+
+
+
+# # f-string
+
+
+# print(f'olá  {10} ')
+
+# # 1 Imprima uma mensagem de boas-vindas na tela.
+
+# print('olá')
+
+
+# 2 Declare uma variável booleana verdadeiro com o valor True e imprima seu tipo
+
+# zero = bool(input ('digite um número: '))
+
+# print(type(zero))
+
+
+# 3 Imprima o resultado da multiplicação de dois números decimais de sua escolha
+
+# n1 = float(input('coloque um decimal aqui: '))
+# n2 = float(input('coloque outro: '))
+
+# resposta = (n1 * n2)
+# print('resposta da multiplicção dos sois: ')
+# print(resposta)
+
+# 4 Imprima o resultado da divisão (/)de dois números inteiros de sua escolha.
+
+# n1 = int(input('coloque um número inteiro: '))
+# n2 = int(input('coloque outro: '))
+
+# result = (n1 / n2)
+# print(result)
+
+
+# 5 Imprima o resultado da subtração de dois números inteiros de sua escolha
+
+# n1 = int(input('coloque um número inteiro: '))
+# n2 = int(input('coloque outro: '))
+
+# result = (n1 - n2)
+# print(result)
+
+# 6 Imprima o resultado da divisão (//)inteira de dois números inteiros 
+# de sua escolha.
+
+# n1 = int(input('coloque um número inteiro: '))
+# n2 = int(input('coloque outro: '))
+
+# result = (n1 // n2)
+# print(result)
+
+# 7 Imprima o resultado da multiplicação de 4 números 
+# decimais de sua escolha
+
+# n1 = float(input('coloque um número decimal: '))
+# n2 = float(input('coloque outro: '))
+# n3 = float(input('mais outro: '))
+# n4 = float(input('e mais outro: '))
+# result = (n1 * n2  * n3 * n4)
+# print(result)
+
+# 8 Declare uma variável numero e atribua um número inteiro. Em seguida, 
+# imprima o dobro desse número
+
+# numero = 5
+# ninteiro = int(input('coloque um número inteiro: '))
+
+# result = (numero + ninteiro)**2
+
+# print(result)
+
+
+# 9 Crie um sistema de cadastro com as estruturas que vc já 
+# conhece(Use apenas input, print e variavel)
+# # nome, email, idade, cidade, gradução, estado civil 
+
+print('bem vindo ao sistema de cadastros')
+
+nome = input('digite o seu nome: ')
+
+email = input('digite o seu email: ')
+
+idade = input('digite a sua idade: ')
+
+cidade = input('digite a sua cidade: ')
+
+graduação = input('digite a sua graduação: ')
+
+estado_civil = input(' digite o seu estado civil')
+
+cadastro_final = (nome , email , idade , cidade , graduação , estado_civil)
