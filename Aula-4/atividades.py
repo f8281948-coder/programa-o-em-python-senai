4d431f8c05aa49208198a3fcb8648bd0800e778d
f8281948-coder <f8281948@gmail.com>
2026-07-21 21:27:18 -0300

    Add files via upload

---
+++ Aula-4/atividades.py
@@ -0,0 +1,51 @@
+#1 - Crie um programa para efetuar a leitura de um número inteiro e apresentar o resultado do quadrado deste número.
+
+# numero_1 = int(input('número1 '))
+# numero_2 = int(input('número2 '))
+
+# resultado = (((numero_1) + (numero_2))**2)
+# print('resposta')
+# print(resultado)
+
+
+#2 - Crie duas variáveis para armazenar seu primeiro nome e sobrenome. Em seguida, concatene-as para formar seu nome completo e exiba o resultado.
+
+# nome = input('nome; ')
+# sobrenome = input('sobrenome; ')
+
+# nome_completo = (nome  + " " + sobrenome)
+
+# print('nome completo:')
+# print(nome_completo)
+
+#### 3 - Peça ao usuário para digitar dois números inteiros e armazene-os em variáveis. Realize a concatenação desses números como strings e exiba o resultado.
+
+# print('digite dois números inteiros')
+# numero1 = int(input('numero 1: '))
+# numero2 = int(input('numero 2: '))
+
+# resultado = str(numero1 + numero2)
+# print(resultado)
+
+
+#### 4 - Crie uma variável para armazenar a palavra "Python". Em seguida, adicione um número inteiro ao final da palavra usando a concatenação e exiba o resultado.
+
+# palavra = input('digite a palavra: ')
+# numero = input('digite um número: ')
+
+# juncao = (palavra + " " + numero)
+
+# print(juncao)
+
+
+#5 - Declare uma variável contendo uma frase. Em seguida, peça ao usuário para digitar uma palavra e concatene essa palavra no final da frase. Exiba o resultado. 
+
+frase = input('digite uma frase: ')
+Palavra = input('digite uma palavra: ')
+
+formacao = (frase + " " + Palavra)
+
+print(formacao)
+
+Frase = str('programe')
+print(Frase)
