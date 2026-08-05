8ea8cf878fcabfdf8665f59ce525869a93dfc3e5
f8281948-coder <f8281948@gmail.com>
2026-08-04 21:56:17 -0300

    Add files via upload

---
+++ Aula-13/main.py
@@ -0,0 +1,45 @@
+import aula13.modulo as md
+
+## EXERCÍCIOS:
+
+# ***Trabalhando um pouco mais com funções | loops | variáveis | listas …***
+
+# ***Crie com módulos das funções utilize parâmetros/return:*** 
+
+# **1 - Crie um número aleatório de 5,10**
+
+print(md.at1())
+
+# **2 - Crie 3 números aleatórios**
+
+print(md.at2())
+
+# **3 - Crie um número aleatório entre 10 a 30 utilize o range()**
+
+print(md.at3())
+
+# **4 - Contagem regressiva simples**
+# Escreva um programa que exiba uma contagem regressiva de 10 a 1, e depois imprima "Fogo!".(loop for)
+
+print(md.at4())
+
+# **5 - Soma de números pares**
+# Peça ao usuário que insira um número inteiro positivo e, em seguida, calcule a soma de todos os números pares de 2 até o número inserido.
+# (use módulo, if, for)
+# ***Utilize print() na saída***
+
+md.at5()
+
+#6 Peça ao usuário para inserir um número inteiro e mostre a tabuada de multiplicação desse número de 1 a 10.
+# (while ou for )
+
+md.at6()
+
+
+# **7 -  Números ímpares reversos**
+
+# Exiba uma contagem regressiva de números ímpares de 99 a 1.
+
+# (for)
+
+print(md.at7())
